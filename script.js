ecbc292f0276d19a957ebad62c73d85fbd9d7c8a
Alberto Blasco <albertoblascoo@gmail.com>
2026-06-30 14:27:17 +0200

    detalles y formularios configurados

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ bocadillo.addEventListener("click", ()=>{
     menu.classList.toggle("active");
     navMobil.classList.toggle("active");
 });
+
+
+emailjs.init("6ejWXDpTYItMERqKq");
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_c18oxe7",
+        "template_rr4o3wp",
+        this
+    )
+    .then(() => {
+
+        alert("Solicitud enviada correctamente.");
+
+        formulario.reset();
+
+    })
+    .catch(() => {
+        alert("Ha ocurrido un error.");
+
+    });
+
+});
